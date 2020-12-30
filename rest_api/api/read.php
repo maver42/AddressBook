2ab9373e653bf9c80d76ddf33859c4d4576059e2
Anja <anja.m13@gmail.com>
2020-12-30 19:34:30 +0100

    POST request

--- rest_api/api/read.php
+++ rest_api/api/read.php
@@ -1,30 +1,34 @@
 <?php
-    //Headers
-    header('Access-Control-Allow-Origin: *');
-    header('Content-Type: application/json');
+//Headers
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
 
-    include_once '../config/Database.php';
-    include_once '../models/Person.php';
+include_once '../config/Database.php';
+include_once '../models/Person.php';
 
-    // instanca DB
-    $database = new Database();
-    $db = $database->connect();
+// instanca DB
+$database = new Database();
+$db = $database->connect();
 
-    // instanca person
-    $person = new Person($db);
+// instanca person
+$person = new Person($db);
 
+//--------------------------------------------------------------
+//   GET ALL
+//--------------------------------------------------------------
+if (!isset($_GET['id'])) { // ne sme bit vpisan id
     // person query
     $result = $person->read();
     // row count (number of persons in DB)
     $num = $result->rowCount();
-  
 
-    if($num > 0){
+
+    if ($num > 0) {
         //person array
         $people = array();
         $people['data'] = array();
 
-        while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
             extract($row); //zato da ne treba pisati $row['title']
 
             $person_col = array(
@@ -42,5 +46,32 @@
         //convert to json
         echo json_encode($people);
     } else {
-        echo json_encode( array('message'=> 'No person found'));
+        echo json_encode(array('message' => 'No person found'));
     }
+}
+
+//--------------------------------------------------------------
+//  GET SINGLE PERSON
+//--------------------------------------------------------------
+//get id for single person
+if (isset($_GET['id']) && $_GET['id'] != null ) {
+    
+    $person->id = $_GET['id'];
+
+    $person->readById();
+    
+    $person_arr = array(
+        'id' => $person->id,
+        'firstname' => $person->firstName,
+        'lastname' => $person->lastName,
+        'mail' => $person->mail,
+        'phone' => $person->phone,
+    );
+
+    //make json
+    print_r(json_encode($person_arr));
+}
+else{
+    //id ni vpisan
+    die();
+}
--- rest_api/models/Person.php
+++ rest_api/models/Person.php
@@ -6,8 +6,8 @@ class Person {
 
     //person properties
     public $id;
-    public $firstName;
-    public $lastName;
+    public $firstname;
+    public $lastname;
     public $mail;
     public $phone;
 
@@ -26,6 +26,57 @@ class Person {
         $stmt->execute();
 
         return $stmt;
+    }
+
+    public function readById(){
+        //query
+        $query = 'SELECT * FROM ' . $this->table . ' WHERE id= ?';
+        //prepere query
+        $stmt = $this->conn->prepare($query);
+
+        //bind id
+        $stmt->bindParam(1, $this->id);
+
+        //execute query
+        $stmt->execute();
+
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        $this->id = $row['id'];
+        $this->firstName = $row['firstname'];
+        $this->lastName = $row['lastname'];
+        $this->mail = $row['mail'];
+        $this->phone = $row['phone'];
+
+        return $stmt;
+    }
+
+    //Create new person
+    public function create(){
+        $query = 'INSERT INTO ' . $this->table . ' SET firstname = :firstname, lastname = :lastname, mail = :mail, phone = :phone';
+
+        $stmt = $this->conn->prepare($query);
+
+        //clean input
+        $this->firstname = htmlspecialchars(strip_tags($this->firstname));
+        $this->lastname = htmlspecialchars(strip_tags($this->lastname));
+        $this->mail = htmlspecialchars(strip_tags($this->mail));
+        $this->phone = htmlspecialchars(strip_tags($this->phone));
+
+        //bind data
+        $stmt->bindParam(':firstname', $this->firstname);
+        $stmt->bindParam(':lastname', $this->lastname);
+        $stmt->bindParam(':mail', $this->mail);
+        $stmt->bindParam(':phone', $this->phone);
+
+        if($stmt->execute()) {
+            return true;
+        }
+
+        //in case smth goes south
+        printf("Error: %s. \n", $stmt->error);
+
+        return false;
     }
 
 }
